98456643491a594d6d80e5c6d5ebd9f4558ae074
IrenaMartty <irenamartincevic3@gmail.com>
2024-02-18 23:59:07 +0100

    Agregadas las funciones de la filtracion

--- Desktop/FrontEnd BootCamp/Modul 4 - Asincronismo/ADA_Buscador_de_comics_TP/app.js
+++ Desktop/FrontEnd BootCamp/Modul 4 - Asincronismo/ADA_Buscador_de_comics_TP/app.js
@@ -31,7 +31,7 @@ const getMComics = async(title) => {
     const url = `${urlBase}comics?${ts}${publicKey}${hash}${showTitle}`
     const response = await fetch(url)
     const data = await response.json()
-console.log(data.data.results)
+// console.log(data.data.results)
 return(data.data.results)
 }
 
@@ -40,7 +40,7 @@ const getMCharacters = async(name) => {
   const url = `${urlBase}characters?${ts}${publicKey}${hash}${showName}`
   const response = await fetch(url)
     const data = await response.json()
-console.log(data.data.results)
+// console.log(data.data.results)
 return(data.data.results)
 
 }
@@ -77,7 +77,6 @@ const printDataCharacters = async(name) => {
   try{
 
     const characters = await getMCharacters(name)
-    console.log(characters);
   
    const showResultsContainer = $("#show-results");
     cleanContainer(showResultsContainer); 
@@ -98,25 +97,147 @@ const printDataCharacters = async(name) => {
     console.error("Error:", error);
   }
 };
-// printDataCharacters()
+printDataCharacters()
 
 
 /* FILTER DATA */
+// // elementos de funcionalidad
+const $order = $("#order")
+const $type = $("#type")
+const $search = $("#search-text")
+const $searchButton = $("#button-search")
+
+// // viariables de control
+let offset = 0
+let type = "comics"
+let orderBy = "title"
+let search = ""
+
+// // Pages
+const $first = $("#first-page");
+const $prev = $("#previous-page");
+const $next = $("#next-page");
+const $last = $("#last-page");
 
 // Text search 
-$(".search-text").addEventListener("input", async() => {
-  const searchText = $(".search-text").value;
-  console.log("Search Text:", searchText);
-  await printDataComics(searchText);
-  await totalResultsNum()
+// $(".search-text").addEventListener("input", async() => {
+//   const searchText = $(".search-text").value;
+//   await printDataComics(searchText);
+//   await totalResultsNum()
+// })
+
+let dataComics = [];
+
+async function getApiData() {
+  try {
+      let orderByParam;
+      if (orderBy === "modified") {
+          orderByParam = "-modified";
+      } else {
+          orderByParam = type === "comics" ? (orderBy || "title") : (orderBy ? orderBy : "name");
+      }
+      const response = await fetch(
+          `https://gateway.marvel.com/v1/public/${type}?${ts}${publicKey}${hash}&offset=${offset}&orderBy=${orderByParam}${search && `&${type == "comics" ? "title" : "name"}StartsWith=${search}`}`
+      );
+      const data = await response.json();
+      dataComics = data.data.results;
+      render();
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function render() {
+  const showResultsContainer = $("#show-results");
+  cleanContainer(showResultsContainer);
+  let html = '';
+  
+  if (type === "comics") {
+    dataComics.forEach((comic) => {
+      html += `<div onClick="getDetailData(${comic.id})">${comic.title}</div>`;
+    });
+  } else if (type === "characters") {
+    dataComics.forEach((character) => {
+      html += `<div onClick="getDetailData(${character.id})">${character.name}</div>`;
+    });
+  }
+  
+  showResultsContainer.innerHTML = html;
+}
+
+$order.onchange = function(e) {
+  if(type === "comics" && e.target.value === "name") {
+    orderBy = "title"
+  } else if (type == "comics" && e.target.value == "-name") {
+    orderBy = "-title";
+    return;
+  } else {
+    orderBy = e.target.value;
+  }
+  console.log(orderBy);
+
+}
+
+$type.onchange = function(e) {
+  type = e.target.value
+  if (type == "comics" && orderBy == "name") {
+    orderBy = "title";
+    return;
+  } else if (type == "comics" && orderBy == "-name") {
+    orderBy = "-title";
+    return;
+  } else {
+    orderBy = "name";
+  }
+  console.log(orderBy);
+}
+
+$search.onchange = function(e) {
+  search = e.target.value
+}
+
+$searchButton.onclick = function(e) {
+getApiData()
+}
+
+window.addEventListener("load", () => {
+  getApiData()
 })
-// Type filter
 
-
-// Order filters
-
-
+async function getDetailData(id) {
+  try {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/${type}/${id}`
+    );
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    dataComics = data.data.results;
+  } catch (error) {
+    console.log(error);
+  }
+}
 /* PAGE NUMBER */
+
+$prev.onclick = function(e) {
+  offset -= 20
+  
+}
+
+$next.onclick = function(e) {
+  offset += 20
+}
+
+const totalResultsNum = async() => {
+  try {
+    const comics = await getMComics();
+    const totalResults = comics.length; 
+    $(".total-number").innerHTML = `${totalResults}`;
+  } catch (error) {
+    console.error("Error fetching total results:", error);
+  }
+};
+totalResultsNum()
 
 // const pageNumber = async(pageNum) => {
 //     const result = await pageNum
@@ -135,13 +256,18 @@ $(".search-text").addEventListener("input", async() => {
 
 
 // }
-const totalResultsNum = async () => {
-  try {
-    const comics = await getMComics();
-    const totalResults = comics.length; 
-    $(".total-number").innerHTML = `${totalResults}`;
-  } catch (error) {
-    console.error("Error fetching total results:", error);
-  }
-};
-totalResultsNum()
+
+
+// const numberOfPages = async() => {
+//   try{
+//     const comics = await getMComics()
+//     const totalNumberOfPages = comics.data.total
+//     $(".number-of-pages").innerHTML = `${totalNumberOfPages}`;
+//   } 
+//   catch (error) {
+//     console.error("Error fetching total results:", error);
+//   }
+// }
+// numberOfPages()
+
+
